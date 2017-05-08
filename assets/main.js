7ceceac5a634999da4bc1fac1352e6b3db36eb39
caroquintana <caro.quintana.n@gmail.com>
2017-05-08 14:13:35 -0400

    avance1

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,13 @@
-alert("Buenos días, buenas tardes");
+//IMC = Peso (kg) / altura (m) 2
+
+
+function calculo_imc(peso, altura) {
+	var altura = document.imc.altura.value;
+	var peso = document.imc.peso.value;
+
+	var alturaAlCuadrado = Math.pow(altura,2);
+	alert(peso/alturaAlCuadrado);
+	
+}
+
+
