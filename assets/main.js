d53fdccb19237f3f524091b0a33332006f17b632
maitc <maibeet.torres@gmail.com>
2017-05-08 15:45:27 -0400

    calculadora Mai

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+function calculadora(){
+
+	var pe = document.getElementById("peso").value;
+	var est = document.getElementById("estatura").value;
+
+	var res = pe / (Math.pow(est, 2));
+	var resultado = res.toFixed(2);
+	alert(resultado);
+
+}
+
+calculadora();
