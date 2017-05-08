4593e200ff64ac2a57babed38a29ce3e77f40fd9
Anayn Pavez <nanyn.pavez@gmail.com>
2017-05-08 15:54:08 -0300

    version funcional

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+
+var espacioDonde = document.getElementById('espacio');
+var res = document.getElementById('respuesta');
+
+/*function calcular(){
+var peso = document.getElementById('peso').value;	
+var altura = document.getElementById('altura').value;
+
+res.innerHTML = peso/(Math.pow(altura, 2));
+}*/
+
+function calcular(){
+	var peso = document.getElementById('peso').value;	
+	var altura = document.getElementById('altura').value;
+
+	return alert(peso/(Math.pow(altura, 2)));
+}
+
+
+
